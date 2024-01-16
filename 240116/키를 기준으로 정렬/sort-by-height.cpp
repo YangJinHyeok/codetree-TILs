18c53e8b09f164251c4531eee97688bd6eca96e0
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-01-16 19:09:14 +0900

    Title: 키를 기준으로 정렬, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240116/키를 기준으로 정렬/sort-by-height.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+class Student {
+    public:
+        string name;
+        int height, weight;
+
+        Student(string name, int height, int weight) {
+            this->name = name;
+            this->height = height;
+            this->weight = weight;
+        }
+
+        Student(){}
+};
+
+bool cmp(Student a, Student b) { // 국어 점수 기준 내림차순 정렬
+    return a.height < b.height; 
+}
+
+int main() {
+    int a;
+    cin >> a;
+    Student students[a];
+
+    for(int i = 0; i < a; i++){
+        string b;
+        int c, d;
+        cin >> b >> c >> d;
+        students[i] = Student(b,c,d);
+    }
+
+
+    sort(students, students + 5, cmp);
+
+    for(int i = 0; i < 5; i++)
+        cout << students[i].name << " " << students[i].height << " " << students[i].weight << endl;
+
+    return 0;
+}
