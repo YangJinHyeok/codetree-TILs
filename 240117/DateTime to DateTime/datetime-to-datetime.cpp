a5a87edcf312b196773e391432bc1e0440f10508
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-01-17 18:25:26 +0900

    Title: DateTime to DateTime, Time: 1063ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240117/DateTime to DateTime/datetime-to-datetime.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int day = 11, hour = 11, min = 11, a, b, c;
+    int elapsed_time = 0;
+    cin >> a >> b >> c;
+    while(true) {
+        if(day == a && hour == b && min == c)
+            break;
+    
+        elapsed_time++;
+        min++;
+    
+        if(min == 60) {
+            hour++;
+            min = 0;
+        }
+        if(hour == 24){
+            day++;
+            hour = 0;
+        }
+    }
+    
+    cout << elapsed_time;
+    
+    return 0;
+}
