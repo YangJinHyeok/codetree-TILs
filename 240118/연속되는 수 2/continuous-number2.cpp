d1696372c3ab40741a0af5e7b55a7b95e3e691a1
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-01-18 22:41:09 +0900

    Title: 연속되는 수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240118/연속되는 수 2/continuous-number2.cpp
+++ 240118/연속되는 수 2/continuous-number2.cpp
@@ -19,14 +19,14 @@ int main() {
             cnt++;
         }
         else if(v[i] != v[i-1]){
-            cnt = 0;
+            cnt = 1;
         }
         if(cnt > max_cnt){
             max_cnt = cnt;
         }
     }
 
-    cout << max_cnt + 1;
+    cout << max_cnt;
 
     return 0;
 }
