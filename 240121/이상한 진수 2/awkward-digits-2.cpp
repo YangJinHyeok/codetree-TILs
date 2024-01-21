b3ede65c5e8a9fab7d15d306e2cc4c411d4f8b72
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-01-21 17:21:03 +0900

    Title: 이상한 진수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240121/이상한 진수 2/awkward-digits-2.cpp
+++ 240121/이상한 진수 2/awkward-digits-2.cpp
@@ -5,16 +5,26 @@ using namespace std;
 int main() {
     string a;
     cin >> a;
-
+    int cnt = 0;
     for(int i = 0; i < a.size(); i++){
         if(a.size() == 1 && a[i] == '1'){
             cout << 0;
             return 0;
         }
+        if(a[i] == '1'){
+            cnt++;
+        }
+    }
+
+    for(int i = 0; i < a.size(); i++){
         if(a[i] == '0'){
             a[i] = '1';
             break;
         }
+    }
+
+    if(cnt == a.size()){
+        a[a.size() - 1] = '0';
     }
 
     int num = 0;
