01259adc3f31034d4a8ccb03e395437c9904092a
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-01-22 18:41:44 +0900

    Title: 최고의 13위치 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240122/최고의 13위치 2/best-place-of-13-2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int arr[20][20];
+
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+
+    int max_cnt = 0;
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n - 2; j++) {
+            for(int k = 0; k < n; k++) {
+                for(int l = 0; l < n - 2; l++) {
+                    if(i == k && abs(j - l) <= 2)//  
+                        continue;
+                    
+                    int cnt1 = arr[i][j] + arr[i][j + 1] + arr[i][j + 2];
+                    int cnt2 = arr[k][l] + arr[k][l + 1] + arr[k][l + 2];
+                    max_cnt = max(max_cnt, cnt1 + cnt2);
+                }
+            }
+        }
+    
+    cout << max_cnt;
+    return 0;
+}
