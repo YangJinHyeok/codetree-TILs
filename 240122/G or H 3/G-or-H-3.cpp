4e073461c92691144a345ba5f8846d11b86d96c1
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-01-22 22:42:29 +0900

    Title: G or H 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240122/G or H 3/G-or-H-3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+char arr[101];
+
+int main() {
+    
+    int n,k;
+    cin >> n >> k;
+    for(int i = 0; i < n; i++){
+        int a;
+        char b;
+        cin >> a >> b;
+        arr[a] = b;
+    }
+    int max_sum = 0;
+    for(int i = 1; i <= 101 - k + 1; i++){
+        int sum = 0;
+        for(int j = i; j <= i + k; j++){
+            if(arr[j] == 'G'){
+                sum += 1;
+            }
+            else if(arr[j] == 'H'){
+                sum += 2;
+            }
+        }
+
+        max_sum = max(max_sum, sum);
+    }
+
+    cout << max_sum;
+
+    return 0;
+}
