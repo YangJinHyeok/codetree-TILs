6697710270a05eee24f558977b0517725f11b917
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-01-25 19:08:26 +0900

    Title: 밭의 높이를 고르게하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240125/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.cpp
+++ 240125/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.cpp
@@ -5,26 +5,22 @@
 #include <climits>
 using namespace std;
 
-bool cmp(const pair<int, int> &a, const pair<int, int> &b){
-    return a.second < b.second;
-}
-
 int main() {
 
-    int n,h,t;
-    vector<pair<int,int>> v;
+    int n, h, t;
+    vector<pair<int, int>> v;
     cin >> n >> h >> t;
 
-    for(int i = 0; i < n; i++){
-        int a,diff;
+    for (int i = 0; i < n; i++) {
+        int a, diff;
         cin >> a;
         diff = abs(h - a);
-        v.push_back({a,diff});
+        v.push_back({ a,diff });
     }
     int min_sum = INT_MAX;
-    for(int i = 0; i < n - t; i++){
+    for (int i = 0; i <= n - t; i++) {
         int sum = 0;
-        for(int j = i; j < i+t; j++){
+        for (int j = i; j < i + t; j++) {
             sum += v[j].second;
         }
 
