045e0ac0da1794189a304562f831a9d96f03b012
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-01-27 23:30:08 +0900

    Title: 개발팀의 능력, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240127/개발팀의 능력/the-capabilities-of-the-development-team.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+using namespace std;
+
+int arr[5];
+
+
+int diff(int i, int j, int k, int l){
+
+    int sum = 0;
+
+    for(int u = 0; u < 5; u++){
+        sum += arr[u];
+    }
+
+    int team1 = arr[i] + arr[j];
+    int team2 = arr[k] + arr[l];
+    int team3 = sum - team1 - team2;
+
+    if(team1 == team2 || team1 == team3 || team2 == team3){
+        return INT_MAX;
+    }
+
+    int max_team = max({team1, team2, team3});
+    int min_team = min({team1, team2, team3});
+
+    return abs(max_team - min_team);
+}
+
+
+
+
+
+int main() {
+    
+    for(int i = 0; i < 5; i++){
+        cin >> arr[i];
+    }
+    int min_ans = INT_MAX;
+    for(int i = 0; i < 5; i++){
+        for(int j = i+1; j < 5; j++){
+
+            for(int k = 0; k < 5; k++){
+                for(int l = k+1; l < 5; l++){
+                    if(i == k || i == l || j == k || j == l){
+                        continue;
+                    }
+
+                    min_ans = min(min_ans, diff(i,j,k,l));
+                }
+            }
+        }
+    }
+
+    cout << min_ans;
+
+    return 0;
+}
