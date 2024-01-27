a3bca78f4df38f3960ff11ec7b598c7b395e0a7d
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-01-28 04:09:47 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240128/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.cpp
+++ 240128/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.cpp
@@ -26,13 +26,13 @@ int main() {
         v.push_back({a,b});
     }
     int ans = INT_MAX;
-    for(int i = 0; i < n - 2; i++){
-        for(int j = i + 1; j < n - 1; j++){
-            for(int k = j + 1; k < n; k++){
-                int xmax = max({v[i].X, v[j].X, v[k].X});
-                int ymax = max({v[i].Y, v[j].Y, v[k].Y});
-                int xmin = min({v[i].X, v[j].X, v[k].X});
-                int ymin = min({v[i].Y, v[j].Y, v[k].Y});
+    if(n == 3){
+        for(int i = 0; i < n - 1; i++){
+            for(int j = i + 1; j < n; j++){
+                int xmax = max({v[i].X, v[j].X});
+                int ymax = max({v[i].Y, v[j].Y});
+                int xmin = min({v[i].X, v[j].X});
+                int ymin = min({v[i].Y, v[j].Y});
 
                 int recsize = (xmax - xmin) * (ymax - ymin);
 
@@ -40,6 +40,23 @@ int main() {
             }
         }
     }
+    else{
+        for(int i = 0; i < n - 2; i++){
+            for(int j = i + 1; j < n; j++){
+                for(int k = j + 1; k < n; k++){
+                    int xmax = max({v[i].X, v[j].X, v[k].X});
+                    int ymax = max({v[i].Y, v[j].Y, v[k].Y});
+                    int xmin = min({v[i].X, v[j].X, v[k].X});
+                    int ymin = min({v[i].Y, v[j].Y, v[k].Y});
+
+                    int recsize = (xmax - xmin) * (ymax - ymin);
+
+                    ans = min(ans, recsize);                
+                }
+            }
+        }
+    }
+    
 
     cout << ans;
 
