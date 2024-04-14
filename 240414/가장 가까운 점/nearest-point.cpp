29803ca6a0eae720258cb2be1dbbce2426d440fc
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-04-14 23:01:10 +0900

    Title: 가장 가까운 점, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240414/가장 가까운 점/nearest-point.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <queue>
+#include <cmath>
+#include <tuple>
+using namespace std;
+
+priority_queue<tuple<int, int, int>> pq;
+
+
+int main() {
+
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        pq.push({ -(a+b),a,b});
+    }
+    for (int i = 0; i < m; i++) {
+        int v, w;
+        tie(ignore, v, w) = pq.top();
+        pq.pop();
+        v += 2;
+        w += 2;
+        pq.push({ -(v + w),v,w });
+    }
+
+    int v, w;
+    tie(ignore, v, w) = pq.top();
+    cout << v << " " << w;
+
+
+
+    return 0;
+}
