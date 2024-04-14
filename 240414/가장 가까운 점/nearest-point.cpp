58dfab61b0b9e68fdb460b9c25f52ac2ee8a09ad
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-04-14 23:06:02 +0900

    Title: 가장 가까운 점, Time: 91ms, Memory: 1MB, Status: Passed - CodeTree

--- 240414/가장 가까운 점/nearest-point.cpp
+++ 240414/가장 가까운 점/nearest-point.cpp
@@ -14,20 +14,22 @@ int main() {
     for (int i = 0; i < n; i++) {
         int a, b;
         cin >> a >> b;
-        pq.push({ -(a+b),a,b});
+        pq.push({ -(a+b),-a,-b});
     }
     for (int i = 0; i < m; i++) {
         int v, w;
         tie(ignore, v, w) = pq.top();
+        v = -v;
+        w = -w;
         pq.pop();
         v += 2;
         w += 2;
-        pq.push({ -(v + w),v,w });
+        pq.push({ -(v + w),-v,-w });
     }
 
     int v, w;
     tie(ignore, v, w) = pq.top();
-    cout << v << " " << w;
+    cout << -v << " " << -w;
 
 
 
