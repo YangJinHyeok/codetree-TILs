af4231da6a37825db19687f7329c246f6b75983d
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-04-15 17:38:15 +0900

    Title: 배열 추출 2, Time: 108ms, Memory: 0MB, Status: Passed - CodeTree

--- 240415/배열 추출 2/array-extraction-2.cpp
+++ 240415/배열 추출 2/array-extraction-2.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
 #include <queue>
+#include <cmath>
+#include <tuple>
 using namespace std;
 
-priority_queue<int> pq;
+priority_queue<pair<int,int>> pq;
 
 int main() {
     int n;
     cin >> n;
-    for(int i =0; i<n;i++){
+    for (int i = 0; i < n; i++) {
         int x;
         cin >> x;
-        if(x != 0){
-            pq.push(-x);
+        if (x != 0) {
+            pq.push({ -abs(x), -x });
         }
-        else{
-            if(pq.size() == 0){
+        else {
+            if (pq.size() == 0) {
                 cout << 0 << "\n";
             }
-            else{
-                cout << -pq.top() << "\n";
+            else {
+                int b = pq.top().second;
+                cout << -b << "\n";
                 pq.pop();
             }
         }
