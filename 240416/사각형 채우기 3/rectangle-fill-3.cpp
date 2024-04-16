bf3bb68052f55fe392154c846dd3cbda78362e46
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-04-16 23:21:30 +0900

    Title: 사각형 채우기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240416/사각형 채우기 3/rectangle-fill-3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+#define MAX 1000
+#define LOL 1000000007
+
+long long arr[MAX + 1];
+
+
+int main() {
+    
+    int n;
+    cin >> n;
+
+    arr[0] = 0;
+    arr[1] = 2;
+
+    for(int i = 2; i <= n; i++) {
+        arr[i] = (arr[i - 1] * 2 + arr[i - 2] * 3) % LOL;
+        for(int j = i - 3; j >= 0; j--)
+            arr[i] = (arr[i] + arr[j] * 2) % LOL;
+    }
+
+    cout << arr[n];
+
+    return 0;
+}
