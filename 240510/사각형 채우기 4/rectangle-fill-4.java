e9bb6ce2a269a3db790d1483e146674a7eb4cf1d
JinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-05-10 18:57:07 +0900

    Title: 사각형 채우기 4, Time: 84ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240510/사각형 채우기 4/rectangle-fill-4.java
@@ -0,0 +1,42 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        final int MOD = 10_007;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+
+        //0 : 안튀어나왔다 1 : 튀어나왔다.
+        int[][][][] dp = new int[N + 2][2][2][2];
+
+        dp[1][0][0][0] = 1;
+        for(int i = 0; i <= N; i++) {
+                //dp[0][0][0]
+                dp[i+1][1][0][0] += dp[i][0][0][0] % MOD;
+                dp[i+1][0][0][1] += dp[i][0][0][0] % MOD;
+                dp[i+1][1][1][1] += dp[i][0][0][0] % MOD;
+                //dp[0][0][1]
+                dp[i+1][1][1][0] += dp[i][0][0][1] % MOD;
+                dp[i+1][0][0][0] += dp[i][0][0][1] % MOD;
+                //dp[0][1][0]
+                dp[i+1][1][0][1] += dp[i][0][1][0] % MOD;
+                //dp[1][0][0]
+                dp[i+1][0][1][1] += dp[i][1][0][0] % MOD;
+                dp[i+1][0][0][0] += dp[i][1][0][0] % MOD;
+                //dp[1][0][1]
+                dp[i+1][0][1][0] += dp[i][1][0][1] % MOD;
+                //dp[1][1][0]
+                dp[i+1][0][0][1] += dp[i][1][1][0] % MOD;
+                //dp[1][1][1]
+                dp[i+1][0][0][0] += dp[i][1][1][1] % MOD;
+        }
+
+        System.out.println(dp[N+1][0][0][0]);
+
+    }
+}
