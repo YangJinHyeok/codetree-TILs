6846ca1fc6579c84178181dee916e7900b5c6df0
JinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-05-31 21:21:58 +0900

    Title: 1이 되는 순간까지, Time: 194ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240531/1이 되는 순간까지/until-the-moment-I-reach-one.js
@@ -0,0 +1,19 @@
+let fs = require("fs");
+let input = fs.readFileSync(0).toString();
+let answer = 0;
+
+function main(n){
+    if(n === 1) return answer;
+    if(n % 2 === 0){
+        answer++;
+        return main(parseInt(n / 2));
+    }
+    else{
+        answer++;
+        return main(parseInt(n / 3));
+    }
+    
+}
+
+main(Number(input));
+console.log(answer);
