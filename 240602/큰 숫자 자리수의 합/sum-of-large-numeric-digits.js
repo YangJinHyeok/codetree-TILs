0dceef059b2c0e31e63182e59f5a735bbac2b6aa
JinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-06-02 17:13:07 +0900

    Title: 큰 숫자 자리수의 합, Time: 196ms, Memory: 14MB, Status: Passed - CodeTree

--- 240602/큰 숫자 자리수의 합/sum-of-large-numeric-digits.js
+++ 240602/큰 숫자 자리수의 합/sum-of-large-numeric-digits.js
@@ -1,4 +1,4 @@
-let fs = require("fs");
+/* let fs = require("fs");
 let input = fs.readFileSync(0).toString().split(" ").map(Number);
 let arr = (input[0] * input[1] * input[2]).toString().split("").map(Number);
 
@@ -8,4 +8,17 @@ function main(n){
 }
 
 let result = main(arr.length - 1);
-console.log(result);
+console.log(result); */
+
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split(" ");
+const number = input[0] * input[1] * input[2];
+
+function sum(n) {
+    if(n < 10) {
+        return n;
+    }
+    return sum(Math.floor(n / 10)) + sum(n % 10);
+}
+
+console.log(sum(number));
