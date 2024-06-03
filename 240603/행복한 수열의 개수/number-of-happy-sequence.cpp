70ffeac9223cce193c18ef78f41b45bd78b93f28
JinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-06-03 14:42:48 +0900

    Title: 행복한 수열의 개수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240603/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 240603/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-#define MAX 100
+#define MAX 101
 
 int arr[MAX][MAX];
 int n,m;
@@ -16,6 +16,9 @@ void garo(){
         for(int j = 0; j < n-1; j++){
             if(arr[i][j] == arr[i][j+1]){
                 cnt++;
+            }
+            else{
+                cnt = 1;
             }
             max_cnt = max(max_cnt, cnt);
         }
@@ -37,6 +40,9 @@ void sero(){
         for(int j = 0; j < n-1; j++){
             if(arr[j][i] == arr[j+1][i]){
                 cnt++;
+            }
+            else{
+                cnt = 1;
             }
             max_cnt = max(max_cnt, cnt);
         }
