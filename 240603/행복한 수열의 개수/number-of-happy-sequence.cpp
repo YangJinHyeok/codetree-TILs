24f7652426fb42dfa9e769c24e01dd41700ce416
JinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-06-03 14:37:17 +0900

    Title: 행복한 수열의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240603/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+#define MAX 100
+
+int arr[MAX][MAX];
+int n,m;
+int ans = 0;
+void garo(){
+    for(int i = 0; i < n; i++){
+        int max_cnt = 0;
+        int cnt = 1;
+        for(int j = 0; j < n-1; j++){
+            if(arr[i][j] == arr[i][j+1]){
+                cnt++;
+            }
+            max_cnt = max(max_cnt, cnt);
+        }
+        if(max_cnt >= m){
+            ans++;
+        }
+    }
+}
+
+
+void sero(){
+    for(int i = 0; i < n; i++){
+        int max_cnt = 0;
+        int cnt = 1;
+        for(int j = 0; j < n-1; j++){
+            if(arr[j][i] == arr[j+1][i]){
+                cnt++;
+            }
+            max_cnt = max(max_cnt, cnt);
+        }
+        if(max_cnt >= m){
+            ans++;
+        }
+    }
+}
+
+
+int main() {
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> arr[i][j];
+        }
+    }
+    garo();
+    sero();
+
+    cout << ans;
+    
+
+    return 0;
+}
