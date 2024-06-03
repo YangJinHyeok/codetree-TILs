086618521b642fe62fbaa3205614e1a63853d930
JinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-06-03 14:02:22 +0900

    Title: 중앙값 계산 2, Time: 167ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240603/중앙값 계산 2/get-median-2.js
@@ -0,0 +1,15 @@
+let fs = require("fs");
+let input = fs.readFileSync(0).toString().split("\n");
+let n = Number(input[0]);
+let t = input[1].split(" ").map(Number);
+let answer = [];
+let arr = [];
+for(let i = 0; i < n; i++) arr.push(t[i]);
+
+for(let i = 1; i <= n; i++){
+    if(i % 2 === 1){
+        let temp = arr.slice(0 , i).sort();
+        answer.push(temp[parseInt(temp.length / 2)]);
+    }
+}
+console.log(answer.join(" "));
