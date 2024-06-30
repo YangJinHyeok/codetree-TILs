49fed26a89bbf4540c4f8ce37123818cf33806d0
JinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-06-30 13:52:45 +0900

    Title: 되돌아오기, Time: 199ms, Memory: 47MB, Status: Passed - CodeTree

--- 240630/되돌아오기/come-back.js
+++ 240630/되돌아오기/come-back.js
@@ -1,45 +1,67 @@
-let fs = require("fs");
-let input = fs.readFileSync(0).toString().split("\n");
-let n = Number(input[0]);
-let arr = input.slice(1).map((e)=>e.split(" "));
-let answer = 0;
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
 
-let flag = false;
-let mapper = {
-    'N' : 0,
-    'E' : 1,
-    'S' : 2,
-    'W' : 3
-}
+// 변수 선언 및 입력
+const n = Number(input[0]);
 
-let dx = [0 , 1 , 0 , -1];
-let dy = [1 , 0 , -1 , 0];
-let x = 0;
-let y = 0;
+// 시작 위치를 기록합니다.
+let x = 0, y = 0;
 
-function isCheck(x , y){
-    return x === 0 && y === 0;
-}
+// 동, 서, 남, 북 순으로 dx, dy를 정의합니다.
+const dx = [1, -1, 0, 0], dy = [0, 0, -1, 1];
 
-for(let i = 0; i < arr.length; i++){
-    let [dir , t] = arr[i];
-    let dirNum = mapper[dir];
-    if(flag) break;
+// 답을 저장합니다.
+let ans = -1;
 
-    for(let j = 0; j < Number(t); j++){
-        let nx = x + dx[dirNum];
-        let ny = y + dy[dirNum];
-        if(!isCheck(nx , ny)){
-            x = nx;
-            y = ny;
-            answer++;
-        }
-        else{
-            flag = true;
-            answer++;
-            break; 
+// 지금까지 걸린 시간을 기록합니다.
+let elapsedTime = 0;
+
+
+// dir 방향으로 dist 만큼 이동하는 함수입니다.
+// 만약 시작지에 도달하면 true를 반환합니다.
+function move(moveDir, dist) {
+    for (let i = 0; i < dist; i++) {
+        x += dx[moveDir];
+        y += dy[moveDir];
+        
+        // 이동한 시간을 기록합니다.
+        elapsedTime += 1;
+
+        // 시작지로 다시 돌아오면,
+        // 답을 갱신해줍니다.
+        if (x === 0 && y === 0) {
+            ans = elapsedTime;
+            return true;
         }
     }
+    
+    return false;
 }
-if(flag) console.log(answer);
-else console.log(-1);
+
+// 움직이는 것을 진행합니다.
+for (let i = 1; i <= n; i++) {
+    let [cDir, dist] = input[i].split(' ');
+    dist = Number(dist);
+    
+    // 각 방향에 맞는 번호를 붙여줍니다.
+    let moveDir;
+    if (cDir === 'E') {
+        moveDir = 0;
+    } else if (cDir === 'W') {
+        moveDir = 1;
+    } else if (cDir === 'S') {
+        moveDir = 2;
+    } else {
+        moveDir = 3;
+    }
+
+    // 주어진 방향대로 dist 만큼 위치를 이동해봅니다.
+    const done = move(moveDir, dist);
+
+    // 시작 위치에 도달했다면, 종료합니다.
+    if (done) {
+        break;
+    }
+}
+
+console.log(ans);
