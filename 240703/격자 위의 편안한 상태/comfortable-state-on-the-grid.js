6a43ec2b44838705f9ad14fac828dec4ffd39dc7
JinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-07-03 05:21:29 +0900

    Title: 격자 위의 편안한 상태, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240703/격자 위의 편안한 상태/comfortable-state-on-the-grid.js
@@ -0,0 +1,35 @@
+let fs = require("fs");
+let input = fs.readFileSync(0).toString().split('\n');
+let [n , m] = input[0].split(" ").map(Number);
+let arr = input.slice(1).map((e) => e.split(" ").map(Number));
+
+let range = Array.from({length : n} , ()=> new Array(n).fill(0));
+let answer = 0;
+
+let dx = [1 , 0 , -1 , 0];
+let dy = [0 , -1 , 0 , 1];
+
+function inRange(x , y){
+    return x >= 0 && x < n && y >= 0 && y < n && range[x][y] === 1;
+}
+
+function isChecked(x , y){
+    let cnt = 0;
+    for(let i = 0; i < 4; i++){
+        let nx = x + dx[i];
+        let ny = y + dy[i];
+        if(inRange(nx , ny)) cnt++;
+    }
+    return cnt;
+}
+
+for(let i = 0; i < arr.length; i++){
+    let [x , y] = arr[i];
+    range[x - 1][y - 1] = 1;
+    if(isChecked(x - 1 , y - 1) >= 3){
+        console.log(1);
+    }
+    else{
+        console.log(0);
+    }
+}
