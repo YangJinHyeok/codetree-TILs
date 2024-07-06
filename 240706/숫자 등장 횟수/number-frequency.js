a0f68dcf236eae1bbd6ec10f7666fe2616949eb0
JinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-07-06 15:28:13 +0900

    Title: 숫자 등장 횟수, Time: 353ms, Memory: 35MB, Status: Passed - CodeTree

--- 240706/숫자 등장 횟수/number-frequency.js
+++ 240706/숫자 등장 횟수/number-frequency.js
@@ -1,5 +1,5 @@
 let fs = require("fs");
-let input = fs.readFileSync(0).toString().split("\n");
+let input = fs.readFileSync(0).toString().trim().split("\n");
 let [n ,m] = input[0].split(" ").map(Number);
 let arr = input[1].split(" ").map(Number);
 let findNum = input[2].split(" ").map(Number);
