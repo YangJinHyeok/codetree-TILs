905d69fd6e51baaa2c95616fefc4cc82bcb1de17
JinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-07-06 16:05:19 +0900

    Title: 대응되는 수와 문자, Time: 726ms, Memory: 31MB, Status: Wrong Answer - CodeTree

---
+++ 240706/대응되는 수와 문자/corresponding-numbers-and-characters.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+// 변수 선언 및 입력:
+const [n, m] = input[0].split(' ').map(Number);
+
+const words = [0].concat(input.slice(1, 1 + n));
+const keys = input.slice(1 + n, 1 + n + m);
+
+const stringToNum = new Map();
+
+words.forEach((word, i) => {
+    stringToNum.set(word, i);
+});
+
+keys.forEach(key => {
+    if ('0' <= key && key <= '9')
+        console.log(words[Number(key)]);
+    else
+        console.log(stringToNum.get(key));
+})
