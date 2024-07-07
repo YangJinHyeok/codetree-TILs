450bed6861c3e8a155337a083d54ac13451f6918
JinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-07-07 10:49:09 +0900

    Title: 원소의 합이 0, Time: 5086ms, Memory: 19MB, Status: Time Limit Exceed - CodeTree

--- 240707/원소의 합이 0/the-sum-of-the-elements-is-0.js
+++ 240707/원소의 합이 0/the-sum-of-the-elements-is-0.js
@@ -14,17 +14,19 @@ let map4 = new Map();
 let answer = 0;
 
 for(let i = 0; i < n; i++){
-    map1.set(a1[i] , 1);
-    map2.set(a2[i] , 1);
-    map3.set(a3[i] , 1);
-    map4.set(a4[i] , 1);
+    map1.set(a1[i] , map1.has(a1[i]) ? map1.get(a1[i]) + 1 : 1);
+    map2.set(a2[i] , map2.has(a2[i]) ? map2.get(a2[i]) + 1 : 1);
+    map3.set(a3[i] , map3.has(a3[i]) ? map3.get(a3[i]) + 1 : 1);
+    map4.set(a4[i] , map4.has(a4[i]) ? map4.get(a4[i]) + 1 : 1);
 }
 
-for(let [key1 , value] of map1){
-    for(let [key2 , value] of map2){
-        for(let [key3 , value] of map3){
-            for(let [key4 , value] of map4){
-                if(key1 + key2 + key3 + key4 === 0) answer++;
+for(let [key1 , value1] of map1){
+    for(let [key2 , value2] of map2){
+        for(let [key3 , value3] of map3){
+            for(let [key4 , value4] of map4){
+                if(key1 + key2 + key3 + key4 === 0){
+                    answer += value1 * value2 * value3 * value4
+                }
             }
         }
     }
