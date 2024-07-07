0033a5c4f3ea06631e2dc8a91544c2750420e332
JinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-07-07 10:59:51 +0900

    Title: 원소의 합이 0, Time: 254ms, Memory: 28MB, Status: Passed - CodeTree

--- 240707/원소의 합이 0/the-sum-of-the-elements-is-0.js
+++ 240707/원소의 합이 0/the-sum-of-the-elements-is-0.js
@@ -6,29 +6,23 @@ let a2 = input[2].split(" ").map(Number);
 let a3 = input[3].split(" ").map(Number);
 let a4 = input[4].split(" ").map(Number);
 
-let map1 = new Map();
-let map2 = new Map();
-let map3 = new Map();
-let map4 = new Map();
-
+let map = new Map();
 let answer = 0;
 
-for(let i = 0; i < n; i++){
-    map1.set(a1[i] , map1.has(a1[i]) ? map1.get(a1[i]) + 1 : 1);
-    map2.set(a2[i] , map2.has(a2[i]) ? map2.get(a2[i]) + 1 : 1);
-    map3.set(a3[i] , map3.has(a3[i]) ? map3.get(a3[i]) + 1 : 1);
-    map4.set(a4[i] , map4.has(a4[i]) ? map4.get(a4[i]) + 1 : 1);
-}
 
-for(let [key1 , value1] of map1){
-    for(let [key2 , value2] of map2){
-        for(let [key3 , value3] of map3){
-            for(let [key4 , value4] of map4){
-                if(key1 + key2 + key3 + key4 === 0){
-                    answer += value1 * value2 * value3 * value4
-                }
-            }
-        }
+
+for(let i = 0; i < n; i++){
+    for(let j = 0; j < n; j++){
+        let sum = a1[i] + a2[j];
+        map.set(sum , map.has(sum) ? map.get(sum) + 1 : 1);
     }
 }
+
+for(let i = 0; i < n; i++){
+    for(let j = 0; j < n; j++){
+        let diff = -(a3[i] + a4[j]);
+        if(map.has(diff)) answer += map.get(diff);
+    }
+}
+
 console.log(answer);
