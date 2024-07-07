dabffd659ab451909537d5332adb301f019164b4
JinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-07-08 08:09:36 +0900

    Title: 특별한 문자, Time: 182ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240708/특별한 문자/special-character.js
@@ -0,0 +1,22 @@
+let fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split('\n');
+let str = input[0].split("").sort();
+let flag = false;
+let answer = "";
+
+let map = new Map();
+
+str.forEach((e) =>{
+    map.set(e , map.has(e) ? map.get(e) + 1 : 1);
+});
+
+for(let [key , value] of map){
+    if(value === 1){
+        flag = true;
+        answer = key;
+        break;
+    }
+}
+
+if(flag) console.log(answer);
+else console.log("None");
