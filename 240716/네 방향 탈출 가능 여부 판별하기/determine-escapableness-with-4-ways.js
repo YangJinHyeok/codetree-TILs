e93c74e9b83eda64b706bcaf7b430538837981a2
JinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-07-16 19:48:05 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 228ms, Memory: 17MB, Status: Passed - CodeTree

--- 240716/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.js
+++ 240716/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.js
@@ -1,8 +1,43 @@
+class Queue {
+    constructor() {  // 빈 큐 하나를 생성합니다.
+        this.q = [];
+        this.head = -1; // head는 큐의 가장 첫 원소의 위치 바로 앞을 가리킵니다.
+        this.tail = -1; // tail은 큐의 가장 마지막 원소의 위치를 가리킵니다.
+    }
+
+    push(item) {  // 큐의 맨 뒤에 데이터를 추가합니다.
+        this.q.push(item);
+        this.tail++;
+    }
+
+    empty() {  // 큐가 비어있으면 true를 반환합니다.
+        return (this.head === this.tail);
+    }
+
+    size() {  // 큐에 들어있는 데이터 수를 반환합니다.
+        return (this.tail - this.head);
+    }
+
+    pop() {  // 큐의 맨 앞에 있는 데이터를 반환하고 제거합니다.
+        if (this.empty()) {
+            throw new Error("Queue is empty");
+        }
+        return this.q[++this.head];
+    }
+
+    front() {  // 큐의 맨 앞에 있는 데이터를 제거하지 않고 반환합니다.
+        if (this.empty()) {
+            throw new Error("Queue is empty");
+        }
+        return this.q[this.head + 1];
+    }
+}
+
 let fs =  require("fs");
 let input = fs.readFileSync(0).toString().split('\n');
 let [n , m] = input[0].split(" ").map(Number);
+const queue = new Queue();
 let grid = input.slice(1).map((e) => e.split(" ").map(Number));
-let queue = [];
 let visited = Array.from({length : n} , () => new Array(m).fill(false));
 let flag = false;
 
@@ -21,8 +56,8 @@ function canGo(x , y){
 }
 function bfs(){
 
-    while(queue.length > 0){
-        let [x , y] = queue.shift();
+    while(queue.size() > 0){
+        let [x , y] = queue.pop();
         if(x === n - 1 && y === m - 1){
             flag = true;
             break;
