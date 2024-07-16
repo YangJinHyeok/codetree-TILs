5307276322f353a5a24668f178acaba1ecb1f128
JinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-07-16 20:50:39 +0900

    Title: 갈 수 있는 곳들, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240716/갈 수 있는 곳들/places-can-go.js
+++ 240716/갈 수 있는 곳들/places-can-go.js
@@ -1,22 +1,35 @@
-class Queue{
-    constructor(){
-        this.queue = [];
-        this.head = -1;
-        this.tail = -1;
+class Queue {
+    constructor() {  // 빈 큐 하나를 생성합니다.
+        this.q = [];
+        this.head = -1; // head는 큐의 가장 첫 원소의 위치 바로 앞을 가리킵니다.
+        this.tail = -1; // tail은 큐의 가장 마지막 원소의 위치를 가리킵니다.
     }
-    empty(){
-        return this.head === this.tail;
-    }
-    size(){
-        return this.tail - this.head;
-    }
-    push(item){
-        this.queue.push(item);
+
+    push(item) {  // 큐의 맨 뒤에 데이터를 추가합니다.
+        this.q.push(item);
         this.tail++;
     }
-    pop(){
-        if(this.empty()) console.log(0);
-        return this.queue[++this.head];
+
+    empty() {  // 큐가 비어있으면 true를 반환합니다.
+        return (this.head === this.tail);
+    }
+
+    size() {  // 큐에 들어있는 데이터 수를 반환합니다.
+        return (this.tail - this.head);
+    }
+
+    pop() {  // 큐의 맨 앞에 있는 데이터를 반환하고 제거합니다.
+        if (this.empty()) {
+            throw new Error("Queue is empty");
+        }
+        return this.q[++this.head];
+    }
+
+    front() {  // 큐의 맨 앞에 있는 데이터를 제거하지 않고 반환합니다.
+        if (this.empty()) {
+            throw new Error("Queue is empty");
+        }
+        return this.q[this.head + 1];
     }
 }
 
@@ -24,7 +37,7 @@ let fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split("\n");
 let [n , m] = input[0].split(" ").map(Number);
 let arr = input.slice(1 , n + 1).map((e) => e.split(" ").map(Number));
- let visited = Array.from({length : n} , ()=>new Array(n).fill(false));
+ let visited = Array.from(Array(n), ()=> Array(n).fill(false));
 let startPoint = input.slice(n + 1).map((e) => e.split(" ").map(Number));
 let queue = new Queue();
 let answer = 1;
